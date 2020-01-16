7317043013be216027dd97872fa07e0eeb19d432
kywelch17 <kywelch17@gmail.com>
2020-01-15 22:58:25 -0500

    created app

---
+++ build/precache-manifest.eabbffc6b929022b0d52514e2df7efa5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46aaac089fc283641a1d56cfaa853650",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaa781a612061cc5e286",
+    "url": "/static/css/main.2120eef6.chunk.css"
+  },
+  {
+    "revision": "d1b9b62b84453e7d4547",
+    "url": "/static/js/2.ba3a8bcf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ba3a8bcf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eaa781a612061cc5e286",
+    "url": "/static/js/main.0c9f8069.chunk.js"
+  },
+  {
+    "revision": "452efe56c2036ff11208",
+    "url": "/static/js/runtime-main.83c66be5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
